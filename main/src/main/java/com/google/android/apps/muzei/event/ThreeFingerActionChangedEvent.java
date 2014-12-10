628263772d51608372816b453f20597cc9750935
Devmil <develmil@gmail.com>
2014-12-10 21:03:52 +0100

    Tap action changes

---
+++ main/src/main/java/com/google/android/apps/muzei/event/ThreeFingerActionChangedEvent.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Devmil Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.apps.muzei.event;
+
+/**
+ * Created by devmil on 16.02.14.
+ */
+public class ThreeFingerActionChangedEvent {
+
+    private TapAction mNewAction;
+
+    public ThreeFingerActionChangedEvent(TapAction newAction)
+    {
+        mNewAction = newAction;
+    }
+
+    public TapAction getNewAction() {
+        return mNewAction;
+    }
+}
--- main/src/main/java/com/google/android/apps/muzei/settings/SettingsAdvancedFragment.java
+++ main/src/main/java/com/google/android/apps/muzei/settings/SettingsAdvancedFragment.java
@@ -24,15 +24,22 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 
 import com.google.android.apps.muzei.LockScreenVisibleReceiver;
+import com.google.android.apps.muzei.MuzeiWallpaperService;
 import com.google.android.apps.muzei.NewWallpaperNotificationReceiver;
 import com.google.android.apps.muzei.event.BlurAmountChangedEvent;
 import com.google.android.apps.muzei.event.DimAmountChangedEvent;
+import com.google.android.apps.muzei.event.DoubleTapActionChangedEvent;
 import com.google.android.apps.muzei.event.GreyAmountChangedEvent;
+import com.google.android.apps.muzei.event.TapAction;
+import com.google.android.apps.muzei.event.ThreeFingerActionChangedEvent;
 import com.google.android.apps.muzei.render.MuzeiBlurRenderer;
 
 import net.nurik.roman.muzei.R;
@@ -49,6 +56,8 @@ public class SettingsAdvancedFragment extends Fragment {
     private SeekBar mGreySeekBar;
     private CheckBox mNotifyNewWallpaperCheckBox;
     private CheckBox mBlurOnLockScreenCheckBox;
+    private Spinner mDoubleTapActionSpinner;
+    private Spinner mThreeFingerActionSpinner;
 
     public SettingsAdvancedFragment() {
     }
@@ -121,6 +130,79 @@ public class SettingsAdvancedFragment extends Fragment {
             }
         });
 
+        //Double Tap action
+
+        mDoubleTapActionSpinner = (Spinner)rootView.findViewById(R.id.advanced_settings_doubletapaction_spinner);
+
+        TapActionEntry[] entries = new TapActionEntry[] {
+                new TapActionEntry(getString(R.string.settings_tap_action_nothing), TapAction.Nothing),
+                new TapActionEntry(getString(R.string.settings_tap_action_showoriginal), TapAction.ShowOriginalArtwork),
+                new TapActionEntry(getString(R.string.settings_tap_action_next), TapAction.NextArtwork),
+                new TapActionEntry(getString(R.string.settings_tap_action_view), TapAction.ViewArtwork)
+        };
+
+        ArrayAdapter<TapActionEntry> doubleTapActionSpinnerAdapter =
+                new ArrayAdapter<TapActionEntry>(
+                        inflater.getContext(),
+                        R.layout.settings_ab_spinner_list_item_dropdown,
+                        entries);
+
+        TapAction currentDoubleTapAction = TapAction.fromCode(
+                getSharedPreferences().getInt(
+                        MuzeiWallpaperService.PREF_DOUBLETAPACTION,
+                        TapAction.ShowOriginalArtwork.getCode()));
+
+        initTapActionSpinner(mDoubleTapActionSpinner, doubleTapActionSpinnerAdapter, entries, currentDoubleTapAction);
+
+        mDoubleTapActionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TapActionEntry entry = (TapActionEntry)parent.getSelectedItem();
+                TapAction newAction = entry.getTapAction();
+                getSharedPreferences().edit()
+                        .putInt(MuzeiWallpaperService.PREF_DOUBLETAPACTION, newAction.getCode())
+                        .apply();
+                EventBus.getDefault().post(new DoubleTapActionChangedEvent(newAction));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        //Three finger action
+
+        mThreeFingerActionSpinner = (Spinner)rootView.findViewById(R.id.advanced_settings_threefingertapaction_spinner);
+
+        ArrayAdapter<TapActionEntry> threeFingerActionSpinnerAdapter =
+                new ArrayAdapter<TapActionEntry>(
+                        inflater.getContext(),
+                        R.layout.settings_ab_spinner_list_item_dropdown,
+                        entries);
+
+        TapAction currentThreeFingerAction = TapAction.fromCode(
+                getSharedPreferences().getInt(
+                        MuzeiWallpaperService.PREF_THREEFINGERACTION,
+                        TapAction.Nothing.getCode()));
+
+        initTapActionSpinner(mThreeFingerActionSpinner, threeFingerActionSpinnerAdapter, entries, currentThreeFingerAction);
+
+        mThreeFingerActionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TapActionEntry entry = (TapActionEntry)parent.getSelectedItem();
+                TapAction newAction = entry.getTapAction();
+                getSharedPreferences().edit()
+                        .putInt(MuzeiWallpaperService.PREF_THREEFINGERACTION, newAction.getCode())
+                        .apply();
+                EventBus.getDefault().post(new ThreeFingerActionChangedEvent(newAction));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
         mNotifyNewWallpaperCheckBox = (CheckBox) rootView.findViewById(
                 R.id.notify_new_wallpaper_checkbox);
         mNotifyNewWallpaperCheckBox.setOnCheckedChangeListener(
@@ -133,7 +215,8 @@ public class SettingsAdvancedFragment extends Fragment {
                         EventBus.getDefault().post(new BlurAmountChangedEvent());
 
                     }
-                });
+                }
+        );
         mNotifyNewWallpaperCheckBox.setChecked(getSharedPreferences()
                 .getBoolean(NewWallpaperNotificationReceiver.PREF_ENABLED, true));
 
@@ -152,6 +235,23 @@ public class SettingsAdvancedFragment extends Fragment {
         mBlurOnLockScreenCheckBox.setChecked(!getSharedPreferences()
                 .getBoolean(LockScreenVisibleReceiver.PREF_ENABLED, false));
         return rootView;
+    }
+
+    private void initTapActionSpinner(Spinner tapActionSpinner, ArrayAdapter<TapActionEntry> arrayAdapter, TapActionEntry[] entries, TapAction currentTapAction) {
+        tapActionSpinner.setAdapter(arrayAdapter);
+
+        //Search the current action in the provided entries
+        int idx = 0;
+        for(int i=0; i<entries.length; i++)
+        {
+            if(entries[i].getTapAction() == currentTapAction)
+            {
+                idx = i;
+                break;
+            }
+        }
+        //to set the selected item
+        tapActionSpinner.setSelection(idx);
     }
 
     @Override
@@ -194,4 +294,31 @@ public class SettingsAdvancedFragment extends Fragment {
         }
     };
 
+
+    public class TapActionEntry
+    {
+        private String mName;
+        private TapAction mAction;
+
+        public TapActionEntry(String name, TapAction action)
+        {
+            mName = name;
+            mAction = action;
+        }
+
+        public String getName()
+        {
+            return mName;
+        }
+
+        public TapAction getTapAction()
+        {
+            return mAction;
+        }
+
+        @Override
+        public String toString() {
+            return mName;
+        }
+    }
 }
