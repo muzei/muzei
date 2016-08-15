56cfdbdb3cc61dfdc2186c52eed7f16f1f4bd0c5
Ian Lake <ilake@google.com>
2016-08-15 15:26:45 -0700

    Fix parsing Artwork JSON where DATE_ADDED isn't included

--- api/src/main/java/com/google/android/apps/muzei/api/Artwork.java
+++ api/src/main/java/com/google/android/apps/muzei/api/Artwork.java
@@ -459,7 +459,7 @@ public class Artwork {
                 .attribution(jsonObject.optString(KEY_ATTRIBUTION))
                 .token(jsonObject.optString(KEY_TOKEN))
                 .metaFont(jsonObject.optString(KEY_META_FONT))
-                .dateAdded(new Date(jsonObject.getLong(KEY_DATE_ADDED)));
+                .dateAdded(new Date(jsonObject.optLong(KEY_DATE_ADDED, 0)));
 
         String componentName = jsonObject.optString(KEY_COMPONENT_NAME);
         if (!TextUtils.isEmpty(componentName)) {
