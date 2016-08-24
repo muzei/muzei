4e7a1594cbe137e0a9899a15ff8ca7f66ea230f8
ianhanniballake <ian.hannibal.lake@gmail.com>
2016-08-23 21:19:23 -0700

    Move artwork retries to use JobScheduler on API 21+
    Instead of retrying whenever the network connectivity changes, use JobScheduler to schedule the retry job and backoff logic.

--- main/src/main/java/com/google/android/apps/muzei/NetworkChangeReceiver.java
+++ main/src/main/java/com/google/android/apps/muzei/NetworkChangeReceiver.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
+import android.os.Build;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 import com.google.android.apps.muzei.api.MuzeiContract;
@@ -35,9 +36,11 @@ public class NetworkChangeReceiver extends WakefulBroadcastReceiver {
         if (hasConnectivity) {
             // Check with components that may not currently be alive but interested in
             // network connectivity changes.
-            Intent retryIntent = TaskQueueService.maybeRetryDownloadDueToGainedConnectivity(context);
-            if (retryIntent != null) {
-                startWakefulService(context, retryIntent);
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                Intent retryIntent = TaskQueueService.maybeRetryDownloadDueToGainedConnectivity(context);
+                if (retryIntent != null) {
+                    startWakefulService(context, retryIntent);
+                }
             }
 
             // TODO: wakeful broadcast?
