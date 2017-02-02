6448903caec6f57deeef88b01e27ba6950ec859e
Ian Lake <ilake@google.com>
2017-02-02 14:47:39 -0800

    Remove unneeded import

--- main/src/main/java/com/google/android/apps/muzei/settings/SettingsChooseSourceFragment.java
+++ main/src/main/java/com/google/android/apps/muzei/settings/SettingsChooseSourceFragment.java
@@ -41,7 +41,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
