c3a3c96fceb1d7a9de17ed27213359cf2f2ed706
Ian Lake <ilake@google.com>
2017-02-24 10:11:51 -0800

    Ensure notify events are properly sent out after batch operations

--- source-gallery/src/main/java/com/google/android/apps/muzei/gallery/GalleryProvider.java
+++ source-gallery/src/main/java/com/google/android/apps/muzei/gallery/GalleryProvider.java
@@ -167,6 +167,7 @@ public class GalleryProvider extends ContentProvider {
         try {
             return super.applyBatch(operations);
         } finally {
+            holdNotifyChange = false;
             Context context = getContext();
             if (context != null) {
                 ContentResolver contentResolver = context.getContentResolver();
