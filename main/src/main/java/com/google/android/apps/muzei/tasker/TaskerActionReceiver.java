d1c7b1f894e5e141d62984de6d903a808d1ff96d
ianhanniballake <ian.hannibal.lake@gmail.com>
2017-03-13 21:40:11 -0700

    Adds a Tasker plugin to trigger the 'Next Artwork' action
    As per the Tasker documentation: http://www.twofortyfouram.com/developer

---
+++ main/src/main/java/com/google/android/apps/muzei/tasker/TaskerActionReceiver.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.tasker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.apps.muzei.SourceManager;
+import com.google.android.apps.muzei.api.internal.ProtocolConstants;
+
+import static com.twofortyfouram.locale.api.Intent.ACTION_FIRE_SETTING;
+import static com.twofortyfouram.locale.api.Intent.EXTRA_BUNDLE;
+
+/**
+ * Tasker FIRE_SETTING receiver that fires source actions
+ */
+public class TaskerActionReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        if (intent != null
+                && ACTION_FIRE_SETTING.equals(intent.getAction())) {
+            Bundle bundle = intent.getBundleExtra(EXTRA_BUNDLE);
+            if (bundle != null) {
+                SourceManager.sendAction(context, bundle.getInt(ProtocolConstants.EXTRA_COMMAND_ID));
+            }
+        }
+    }
+}
