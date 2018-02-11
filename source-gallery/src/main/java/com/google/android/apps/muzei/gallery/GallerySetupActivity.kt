7974f115213cfb05e25b6a6fd0e1b9dc52cb2042
Ian Lake <ian.hannibal.lake@gmail.com>
2018-02-10 17:07:36 -0800

    Convert GallerySetupActivity to Kotlin

---
+++ source-gallery/src/main/java/com/google/android/apps/muzei/gallery/GallerySetupActivity.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.gallery
+
+import android.Manifest
+import android.arch.lifecycle.Observer
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.FragmentActivity
+import android.support.v4.content.ContextCompat
+
+import com.google.android.apps.muzei.api.MuzeiArtSource
+
+class GallerySetupActivity : FragmentActivity() {
+
+    companion object {
+        private const val REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE = 1
+        private const val REQUEST_CHOOSE_IMAGES = 2
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GalleryDatabase.getInstance(this).chosenPhotoDao()
+                .chosenPhotos.observe(this, Observer {chosenUris ->
+            run {
+                val numChosenUris = chosenUris?.size ?: 0
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED || numChosenUris > 0) {
+                    // If we have permission or have any previously selected images
+                    setResult(RESULT_OK)
+                    finish()
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE)
+                }
+            }
+        })
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != REQUEST_READ_EXTERNAL_STORAGE_PERMISSION_REQUEST_CODE) {
+            return
+        }
+
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            setResult(RESULT_OK)
+            finish()
+        } else {
+            // Push the user to the GallerySettingsActivity to see inline rationale or just
+            // select individual photos
+            startActivityForResult(Intent(this, GallerySettingsActivity::class.java).apply {
+                if (intent.getBooleanExtra(MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, false)) {
+                    putExtra(MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, true)
+                }
+            }, REQUEST_CHOOSE_IMAGES)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != REQUEST_CHOOSE_IMAGES) {
+            return
+        }
+        // Pass on the resultCode from the GallerySettingsActivity onto Muzei
+        setResult(resultCode)
+        finish()
+    }
+}
