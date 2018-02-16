b86bd712a702ecc6c6bddf2bfa76e7167d2c678d
Ian Lake <ian.hannibal.lake@gmail.com>
2018-02-15 21:14:52 -0800

    Convert ArtworkComplication classes to Kotlin

---
+++ wearable/src/main/java/com/google/android/apps/muzei/complications/ArtworkComplicationProviderService.kt
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.complications
+
+import android.annotation.TargetApi
+import android.app.PendingIntent
+import android.arch.lifecycle.Observer
+import android.content.Intent
+import android.graphics.drawable.Icon
+import android.os.Build
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.wearable.complications.ComplicationData
+import android.support.wearable.complications.ComplicationManager
+import android.support.wearable.complications.ComplicationProviderService
+import android.support.wearable.complications.ComplicationText
+import android.util.Log
+import androidx.content.edit
+import com.google.android.apps.muzei.FullScreenActivity
+import com.google.android.apps.muzei.api.MuzeiContract
+import com.google.android.apps.muzei.room.Artwork
+import com.google.android.apps.muzei.room.MuzeiDatabase
+import com.google.firebase.analytics.FirebaseAnalytics
+import net.nurik.roman.muzei.BuildConfig
+import java.util.*
+
+/**
+ * Provide Muzei backgrounds to other watch faces
+ */
+@TargetApi(Build.VERSION_CODES.N)
+class ArtworkComplicationProviderService : ComplicationProviderService() {
+
+    companion object {
+        private const val TAG = "ArtworkComplProvider"
+
+        internal const val KEY_COMPLICATION_IDS = "complication_ids"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseAnalytics.getInstance(this).setUserProperty("device_type", BuildConfig.DEVICE_TYPE)
+    }
+
+    override fun onComplicationActivated(complicationId: Int, type: Int, manager: ComplicationManager) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Activated $complicationId")
+        }
+        addComplication(complicationId)
+        val bundle = Bundle().apply {
+            putString(FirebaseAnalytics.Param.CONTENT_TYPE, type.toString())
+        }
+        FirebaseAnalytics.getInstance(this).logEvent("complication_artwork_activated", bundle)
+    }
+
+    private fun addComplication(complicationId: Int) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val complications = preferences.getStringSet(KEY_COMPLICATION_IDS, TreeSet())
+        complications.add(Integer.toString(complicationId))
+        preferences.edit { putStringSet(KEY_COMPLICATION_IDS, complications) }
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "addComplication: $complications")
+        }
+        ArtworkComplicationJobService.scheduleComplicationUpdateJob(this)
+    }
+
+    override fun onComplicationDeactivated(complicationId: Int) {
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Deactivated $complicationId")
+        }
+        FirebaseAnalytics.getInstance(this).logEvent("complication_artwork_deactivated", null)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val complications = preferences.getStringSet(KEY_COMPLICATION_IDS, TreeSet())
+        complications.remove(Integer.toString(complicationId))
+        preferences.edit { putStringSet(KEY_COMPLICATION_IDS, complications) }
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Current complications: $complications")
+        }
+        if (complications.isEmpty()) {
+            ArtworkComplicationJobService.cancelComplicationUpdateJob(this)
+        }
+    }
+
+    override fun onComplicationUpdate(complicationId: Int, type: Int, complicationManager: ComplicationManager) {
+        // Make sure that the complicationId is really in our set of added complications
+        // This fixes corner cases like Muzei being uninstalled and reinstalled
+        // (which wipes out our SharedPreferences but keeps any complications activated)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val complications = preferences.getStringSet(KEY_COMPLICATION_IDS, TreeSet())
+        if (!complications.contains(complicationId.toString())) {
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "Update missing id $complicationId")
+            }
+            addComplication(complicationId)
+        }
+        val applicationContext = applicationContext
+        val artworkLiveData = MuzeiDatabase.getInstance(this).artworkDao().currentArtwork
+        artworkLiveData.observeForever(object : Observer<Artwork> {
+            override fun onChanged(artwork: Artwork?) {
+                artworkLiveData.removeObserver(this)
+                if (artwork == null) {
+                    if (BuildConfig.DEBUG) {
+                        Log.d(TAG, "Update no artwork for $complicationId")
+                    }
+                    complicationManager.updateComplicationData(complicationId,
+                            ComplicationData.Builder(ComplicationData.TYPE_NO_DATA).build())
+                    return
+                }
+                val builder = ComplicationData.Builder(type).apply {
+                    val intent = Intent(applicationContext, FullScreenActivity::class.java)
+                    val tapAction = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+                    when (type) {
+                        ComplicationData.TYPE_LONG_TEXT -> {
+                            val title = artwork.title
+                            val byline = artwork.byline
+                            if (title.isNullOrBlank() && byline.isNullOrBlank()) {
+                                // Both are empty so we don't have any data to show
+                                complicationManager.updateComplicationData(complicationId,
+                                        ComplicationData.Builder(ComplicationData.TYPE_NO_DATA).build())
+                                return
+                            } else if (title.isNullOrBlank()) {
+                                // We only have the byline, so use that as the long text
+                                setLongText(ComplicationText.plainText(byline))
+                            } else {
+                                if (!byline.isNullOrBlank()) {
+                                    setLongTitle(ComplicationText.plainText(byline))
+                                }
+                                setLongText(ComplicationText.plainText(title))
+                            }
+                            setTapAction(tapAction)
+                        }
+                        ComplicationData.TYPE_SMALL_IMAGE -> {
+                            setImageStyle(ComplicationData.IMAGE_STYLE_PHOTO)
+                                    .setSmallImage(Icon.createWithContentUri(MuzeiContract.Artwork.CONTENT_URI))
+                            setTapAction(tapAction)
+                        }
+                        ComplicationData.TYPE_LARGE_IMAGE -> setLargeImage(Icon.createWithContentUri(MuzeiContract.Artwork.CONTENT_URI))
+                    }
+                    if (BuildConfig.DEBUG) {
+                        Log.d(TAG, "Updated $complicationId")
+                    }
+                }
+                complicationManager.updateComplicationData(complicationId, builder.build())
+            }
+        })
+    }
+}
