be533a493080cf77aa38bacd9d28cb61bd812613
Ian Lake <ian.hannibal.lake@gmail.com>
2018-03-02 22:40:48 -0800

    Convert Tasker classes to Kotlin

---
+++ main/src/main/java/com/google/android/apps/muzei/tasker/TaskerActionReceiver.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.tasker
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.google.android.apps.muzei.api.internal.ProtocolConstants
+import com.google.android.apps.muzei.sources.SourceManager
+import com.twofortyfouram.locale.api.Intent.ACTION_FIRE_SETTING
+import com.twofortyfouram.locale.api.Intent.EXTRA_BUNDLE
+
+/**
+ * Tasker FIRE_SETTING receiver that fires source actions
+ */
+class TaskerActionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        intent?.takeIf { it.action == ACTION_FIRE_SETTING }?.getBundleExtra(EXTRA_BUNDLE)?.run {
+            SourceManager.sendAction(context, getInt(ProtocolConstants.EXTRA_COMMAND_ID))
+        }
+    }
+}
