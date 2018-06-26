bfa403de7ecd07328ea0babb788e8cc1c25ba54b
Ian Lake <ian.hannibal.lake@gmail.com>
2018-06-25 21:07:46 -0700

    Fix ChooseSourceFragment coroutine race condition

--- main/src/main/java/com/google/android/apps/muzei/sources/ChooseSourceFragment.kt
+++ main/src/main/java/com/google/android/apps/muzei/sources/ChooseSourceFragment.kt
@@ -578,8 +578,9 @@ class ChooseSourceFragment : Fragment() {
                 FirebaseAnalytics.getInstance(requireContext()).logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundleOf(
                         FirebaseAnalytics.Param.ITEM_ID to setupSource.flattenToShortString(),
                         FirebaseAnalytics.Param.CONTENT_TYPE to "sources"))
+                val context = requireContext()
                 launch {
-                    SourceManager.selectSource(requireContext(), setupSource)
+                    SourceManager.selectSource(context, setupSource)
                 }
             }
 
