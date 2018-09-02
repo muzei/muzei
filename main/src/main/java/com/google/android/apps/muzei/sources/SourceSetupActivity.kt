f5f62b5a08c0c07bacfbca5f8d9f0a78d3eda335
Ian Lake <ian.hannibal.lake@gmail.com>
2018-09-01 17:36:29 -0700

    Warn users when they select a Legacy Source
    Try to tell users the downsides of choosing a Legacy Source.

--- main/src/main/java/com/google/android/apps/muzei/sources/SourceSetupActivity.kt
+++ main/src/main/java/com/google/android/apps/muzei/sources/SourceSetupActivity.kt
@@ -17,15 +17,19 @@
 package com.google.android.apps.muzei.sources
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
-
+import android.support.v4.app.DialogFragment
+import android.support.v4.app.Fragment
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import com.google.android.apps.muzei.api.MuzeiArtSource
 import com.google.android.apps.muzei.room.MuzeiDatabase
 import com.google.android.apps.muzei.util.observe
+import net.nurik.roman.muzei.R
 
-class SourceSetupActivity : FragmentActivity() {
+class SourceSetupActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_CHOOSE_SOURCE = 1
@@ -33,20 +37,33 @@ class SourceSetupActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MuzeiDatabase.getInstance(this).sourceDao().currentSource.observe(this) { source ->
-            if (source != null) {
-                setResult(Activity.RESULT_OK)
-                finish()
-            } else {
-                // Push the user to the SourceSettingsActivity to select a source
-                val intent = Intent(this@SourceSetupActivity,
-                        SourceSettingsActivity::class.java)
+        if (savedInstanceState == null) {
+            SourceWarningDialogFragment().show(supportFragmentManager, "warning")
+        }
+    }
 
-                if (getIntent().getBooleanExtra(
-                                MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, false)) {
-                    intent.putExtra(MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, true)
+    override fun onAttachFragment(fragment: Fragment?) {
+        if (fragment is SourceWarningDialogFragment) {
+            fragment.positiveListener = {
+                MuzeiDatabase.getInstance(this).sourceDao().currentSource.observe(this) { source ->
+                    if (source != null) {
+                        setResult(Activity.RESULT_OK)
+                        finish()
+                    } else {
+                        // Push the user to the SourceSettingsActivity to select a source
+                        val intent = Intent(this@SourceSetupActivity,
+                                SourceSettingsActivity::class.java)
+
+                        if (getIntent().getBooleanExtra(
+                                        MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, false)) {
+                            intent.putExtra(MuzeiArtSource.EXTRA_FROM_MUZEI_SETTINGS, true)
+                        }
+                        startActivityForResult(intent, REQUEST_CHOOSE_SOURCE)
+                    }
                 }
-                startActivityForResult(intent, REQUEST_CHOOSE_SOURCE)
+            }
+            fragment.negativeListener = {
+                finish()
             }
         }
     }
@@ -59,5 +76,23 @@ class SourceSetupActivity : FragmentActivity() {
         // Pass on the resultCode from the SourceSettingsActivity onto Muzei
         setResult(resultCode)
         finish()
+    }
+}
+
+class SourceWarningDialogFragment : DialogFragment() {
+    var positiveListener : () -> Unit = {}
+    var negativeListener : () -> Unit = {}
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(requireContext())
+                .setTitle(R.string.source_warning_title)
+                .setMessage(R.string.source_warning_message)
+                .setPositiveButton(R.string.source_warning_positive) { _, _ ->
+                    positiveListener()
+                }
+                .setNegativeButton(R.string.source_warning_negative) { _, _ ->
+                    negativeListener()
+                }
+                .create()
     }
 }
