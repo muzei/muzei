9a7061ea2e800e0f37a318f08ca2af111f0b0880
Ian Lake <ian.hannibal.lake@gmail.com>
2018-09-15 18:19:51 -0700

    Allow Tasker to select any installed provider
    Expand the current Tasker plugin to allow users to not only select the 'Next Artwork' action, but also actions to select any available MuzeiArtProvider.
    
    This does not run any setup activity under the assumption that users have already set up any provider they select and the rarity of setup activities in general.

--- main/src/main/java/com/google/android/apps/muzei/tasker/TaskerActionReceiver.kt
+++ main/src/main/java/com/google/android/apps/muzei/tasker/TaskerActionReceiver.kt
@@ -20,15 +20,33 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.google.android.apps.muzei.sources.SourceManager
+import com.google.android.apps.muzei.sync.ProviderManager
 import com.twofortyfouram.locale.api.Intent.ACTION_FIRE_SETTING
+import com.twofortyfouram.locale.api.Intent.EXTRA_BUNDLE
+import kotlinx.coroutines.experimental.launch
 
 /**
- * Tasker FIRE_SETTING receiver that fires source actions
+ * Tasker FIRE_SETTING receiver that fires a [TaskerAction]
  */
 class TaskerActionReceiver : BroadcastReceiver() {
+
     override fun onReceive(context: Context, intent: Intent?) {
-        if (intent?.action == ACTION_FIRE_SETTING) {
-            SourceManager.nextArtwork(context)
+        if (intent?.action != ACTION_FIRE_SETTING) {
+            return
+        }
+        val selectedAction = TaskerAction.fromBundle(intent.getBundleExtra(EXTRA_BUNDLE))
+        when (selectedAction) {
+            is SelectProviderAction -> {
+                val authority = selectedAction.authority
+                if (context.packageManager.resolveContentProvider(authority, 0) != null) {
+                    val result = goAsync()
+                    launch {
+                        ProviderManager.select(context, authority)
+                        result.finish()
+                    }
+                }
+            }
+            is NextArtworkAction -> SourceManager.nextArtwork(context)
         }
     }
 }
--- main/src/main/java/com/google/android/apps/muzei/tasker/TaskerSettingActivity.kt
+++ main/src/main/java/com/google/android/apps/muzei/tasker/TaskerSettingActivity.kt
@@ -16,22 +16,89 @@
 
 package com.google.android.apps.muzei.tasker
 
-import android.app.Activity
+import android.arch.lifecycle.ViewModelProvider
+import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.google.android.apps.muzei.util.observeNonNull
+import com.twofortyfouram.locale.api.Intent.EXTRA_BUNDLE
 import com.twofortyfouram.locale.api.Intent.EXTRA_STRING_BLURB
 import net.nurik.roman.muzei.R
 
 /**
- * A minimal EDIT_SETTINGS activity for a Tasker Plugin
+ * The EDIT_SETTINGS activity for a Tasker Plugin allowing users to select whether they
+ * want the Tasker action to move to the next artwork or select a particular provider
  */
-class TaskerSettingActivity : Activity() {
+class TaskerSettingActivity : AppCompatActivity() {
+
+    private val viewModelProvider by lazy {
+        ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(application))
+    }
+    private val viewModel by lazy {
+        viewModelProvider[TaskerSettingViewModel::class.java]
+    }
+
+    private val adapter by lazy {
+        ActionAdapter(this)
+    }
+
+    private val dialog: AlertDialog by lazy {
+        AlertDialog.Builder(this, R.style.Theme_Muzei_Dialog)
+                .setTitle(R.string.tasker_setting_dialog_title)
+                .setSingleChoiceItems(adapter, -1) { _: DialogInterface, which: Int ->
+                    val action = adapter.getItem(which) ?: return@setSingleChoiceItems
+                    val intent = Intent().apply {
+                        putExtra(EXTRA_STRING_BLURB, action.text)
+                        putExtra(EXTRA_BUNDLE, action.action.toBundle())
+                    }
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+                .setOnCancelListener {
+                    setResult(RESULT_CANCELED, null)
+                    finish()
+                }
+                .create()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent().apply {
-            putExtra(EXTRA_STRING_BLURB, getString(R.string.action_next_artwork))
+        viewModel.actions.observeNonNull(this) { actions ->
+            adapter.clear()
+            adapter.addAll(actions)
+            if (!dialog.isShowing) {
+                dialog.show()
+            }
         }
-        setResult(Activity.RESULT_OK, intent)
-        finish()
+    }
+
+    override fun onDestroy() {
+        if (dialog.isShowing) {
+            dialog.dismiss()
+        }
+        super.onDestroy()
+    }
+
+    private class ActionAdapter(
+            context: Context
+    ) : ArrayAdapter<Action>(context, R.layout.tasker_action_item, 0) {
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val view = super.getView(position, convertView, parent) as TextView
+            getItem(position)?.let { action ->
+                view.setCompoundDrawablesRelative(
+                        action.icon, null, null, null)
+                view.text = action.text
+            }
+            return view
+        }
     }
 }
