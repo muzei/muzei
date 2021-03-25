d3aaa1bc5f844bd11ba48064374801bd7249cf49
Ian Lake <ian.hannibal.lake@gmail.com>
2021-03-24 22:06:17 -0700

    Switch the example-watchface to the wear-watchface APIs
    Update the `MuzeiExampleWatchface` to use the new `wear-watchface` APIs.

--- example-watchface/src/main/java/com/example/muzei/watchface/MuzeiExampleWatchface.kt
+++ example-watchface/src/main/java/com/example/muzei/watchface/MuzeiExampleWatchface.kt
@@ -20,16 +20,28 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
-import android.os.Bundle
-import android.support.wearable.complications.ComplicationData
-import android.support.wearable.complications.SystemProviders
-import android.support.wearable.complications.rendering.ComplicationDrawable
-import android.support.wearable.watchface.CanvasWatchFaceService
+import android.icu.util.Calendar
 import android.support.wearable.watchface.WatchFaceStyle
 import android.util.Size
 import android.view.Gravity
 import android.view.SurfaceHolder
 import androidx.core.content.ContextCompat
+import androidx.wear.complications.DefaultComplicationProviderPolicy
+import androidx.wear.complications.SystemProviders
+import androidx.wear.complications.data.ComplicationType
+import androidx.wear.watchface.CanvasComplicationDrawable
+import androidx.wear.watchface.CanvasType
+import androidx.wear.watchface.Complication
+import androidx.wear.watchface.ComplicationsManager
+import androidx.wear.watchface.DrawMode
+import androidx.wear.watchface.Renderer
+import androidx.wear.watchface.WatchFace
+import androidx.wear.watchface.WatchFaceService
+import androidx.wear.watchface.WatchFaceType
+import androidx.wear.watchface.WatchState
+import androidx.wear.watchface.complications.rendering.ComplicationDrawable
+import androidx.wear.watchface.style.UserStyleRepository
+import androidx.wear.watchface.style.UserStyleSchema
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -40,38 +52,78 @@ import kotlinx.coroutines.launch
 /**
  * Simple watchface example which loads and displays Muzei images as the background
  */
-class MuzeiExampleWatchface : CanvasWatchFaceService() {
+class MuzeiExampleWatchface : WatchFaceService() {
 
-    override fun onCreateEngine(): CanvasWatchFaceService.Engine {
-        return Engine()
+    override suspend fun createWatchFace(
+        surfaceHolder: SurfaceHolder,
+        watchState: WatchState
+    ): WatchFace {
+        // This example uses a single, fixed complication to show the time and date
+        // rather than manually draw the time
+        val timeComplication = Complication.createBackgroundComplicationBuilder(
+            0,
+            CanvasComplicationDrawable(ComplicationDrawable(this).apply {
+                activeStyle.run {
+                    titleSize = resources.getDimensionPixelSize(R.dimen.title_size)
+                    textSize = resources.getDimensionPixelSize(R.dimen.text_size)
+                }
+                ambientStyle.run {
+                    titleSize = resources.getDimensionPixelSize(R.dimen.title_size)
+                    textSize = resources.getDimensionPixelSize(R.dimen.text_size)
+                }
+            }, watchState),
+            listOf(ComplicationType.SHORT_TEXT),
+            DefaultComplicationProviderPolicy(SystemProviders.TIME_AND_DATE)
+        ).setFixedComplicationProvider(true).build()
+
+        // Now build the components needed for the WatchFace
+        val userStyleRepository = UserStyleRepository(
+            UserStyleSchema(listOf())
+        )
+        val complicationsManager = ComplicationsManager(
+            listOf(timeComplication),
+            userStyleRepository
+        )
+        val renderer = MuzeiExampleRenderer(
+            surfaceHolder,
+            userStyleRepository,
+            watchState,
+            complicationsManager
+        )
+        return WatchFace(
+            WatchFaceType.DIGITAL,
+            userStyleRepository,
+            renderer,
+            complicationsManager
+        ).setLegacyWatchFaceStyle(
+            WatchFace.LegacyWatchFaceOverlayStyle(
+                WatchFaceStyle.PROTECT_STATUS_BAR or
+                        WatchFaceStyle.PROTECT_HOTWORD_INDICATOR,
+                Gravity.TOP or Gravity.CENTER_HORIZONTAL,
+                false)
+        )
     }
 
-    private inner class Engine : CanvasWatchFaceService.Engine() {
+    private inner class MuzeiExampleRenderer(
+        surfaceHolder: SurfaceHolder,
+        userStyleRepository: UserStyleRepository,
+        watchState: WatchState,
+        private val complicationsManager: ComplicationsManager
+    ) : Renderer.CanvasRenderer(
+        surfaceHolder,
+        userStyleRepository,
+        watchState,
+        CanvasType.HARDWARE,
+        32 // as a ~static watchface, we don't need 60fps
+    ) {
         private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
         private val backgroundPaint: Paint = Paint().apply {
             color = ContextCompat.getColor(this@MuzeiExampleWatchface, android.R.color.black)
         }
         private val loader = ArtworkImageLoader(this@MuzeiExampleWatchface)
-        private lateinit var complicationDrawable: ComplicationDrawable
         private var image: Bitmap? = null
 
-        override fun onCreate(holder: SurfaceHolder) {
-            super.onCreate(holder)
-            setWatchFaceStyle(WatchFaceStyle.Builder(this@MuzeiExampleWatchface)
-                    .setStatusBarGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL)
-                    .setViewProtectionMode(WatchFaceStyle.PROTECT_STATUS_BAR or WatchFaceStyle.PROTECT_HOTWORD_INDICATOR)
-                    .build())
-            setDefaultSystemComplicationProvider(0, SystemProviders.TIME_AND_DATE,
-                    ComplicationData.TYPE_SHORT_TEXT)
-            setActiveComplications(0)
-            complicationDrawable = ComplicationDrawable(this@MuzeiExampleWatchface).apply {
-                setBorderStyleActive(ComplicationDrawable.BORDER_STYLE_NONE)
-                setBorderStyleAmbient(ComplicationDrawable.BORDER_STYLE_NONE)
-                setTitleSizeActive(resources.getDimensionPixelSize(R.dimen.title_size))
-                setTextSizeActive(resources.getDimensionPixelSize(R.dimen.text_size))
-                setTitleSizeAmbient(resources.getDimensionPixelSize(R.dimen.title_size))
-                setTextSizeAmbient(resources.getDimensionPixelSize(R.dimen.text_size))
-            }
+        init {
             coroutineScope.launch {
                 loader.artworkFlow.collect { bitmap ->
                     image = bitmap
@@ -80,37 +132,16 @@ class MuzeiExampleWatchface : CanvasWatchFaceService() {
             }
         }
 
-        override fun onSurfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-            super.onSurfaceChanged(holder, format, width, height)
-            loader.requestedSize = Size(width, height)
-            complicationDrawable.setBounds(0, 0, width, height)
-        }
-
-        override fun onComplicationDataUpdate(watchFaceComplicationId: Int, data: ComplicationData?) {
-            complicationDrawable.setComplicationData(data)
-        }
-
-        override fun onDraw(canvas: Canvas, bounds: Rect) {
+        override fun render(canvas: Canvas, bounds: Rect, calendar: Calendar) {
+            loader.requestedSize = Size(canvas.width, canvas.height)
+            val isInAmbientMode = renderParameters.drawMode == DrawMode.AMBIENT
             image?.takeUnless { isInAmbientMode }?.let { image ->
                 canvas.drawBitmap(image, ((canvas.width - image.width) / 2).toFloat(),
                         ((canvas.height - image.height) / 2).toFloat(), null)
             } ?: run {
                 canvas.drawRect(bounds, backgroundPaint)
             }
-            complicationDrawable.draw(canvas)
-        }
-
-        override fun onAmbientModeChanged(inAmbientMode: Boolean) {
-            complicationDrawable.setInAmbientMode(inAmbientMode)
-            invalidate()
-        }
-
-        override fun onPropertiesChanged(properties: Bundle) {
-            complicationDrawable.setLowBitAmbient(
-                    properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false))
-            complicationDrawable.setBurnInProtection(
-                    properties.getBoolean(PROPERTY_BURN_IN_PROTECTION, false))
-            invalidate()
+            complicationsManager[0]?.render(canvas, calendar, renderParameters)
         }
 
         override fun onDestroy() {
