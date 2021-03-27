9a35e4ed09cffc7c786ad5d3ffac4cae14d9bdfc
Ian Lake <ian.hannibal.lake@gmail.com>
2021-03-27 11:23:51 -0700

    Avoid passing CoroutineScope to collectIn's action, use repeatOnLifecycle
    - Ensures that the action lambda provided by `collectIn` mirrors that of `collect`. Namely, that it does not provide a `CoroutineScope` receiver, thus requiring developers be explicit if they want to create one.
    - Swaps out uses of the higher level `addRepeatingJob` for the lower level primitive of `repeatOnLifecycle`.

--- extensions/src/main/java/com/google/android/apps/muzei/util/FlowExt.kt
+++ extensions/src/main/java/com/google/android/apps/muzei/util/FlowExt.kt
@@ -2,16 +2,19 @@ package com.google.android.apps.muzei.util
 
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.addRepeatingJob
-import kotlinx.coroutines.CoroutineScope
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
 /**
- * A convenience wrapper around [addRepeatingJob] that simply calls [collect]
- * with [action]. Think of it as [kotlinx.coroutines.flow.launchIn], but for collecting.
+ * A convenience wrapper around [androidx.lifecycle.LifecycleCoroutineScope.launch]
+ * that calls [collect] with [action], all wrapped in a lifecycle-aware
+ * [repeatOnLifecycle]. Think of it as [kotlinx.coroutines.flow.launchIn], but for
+ * collecting.
  *
  * ```
  * uiStateFlow.collectIn(owner) { uiState ->
@@ -23,16 +26,18 @@ inline fun <T> Flow<T>.collectIn(
     owner: LifecycleOwner,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
     coroutineContext: CoroutineContext = EmptyCoroutineContext,
-    crossinline action: suspend CoroutineScope.(T) -> Unit
-) = owner.addRepeatingJob(minActiveState, coroutineContext) {
-    collect {
-        action(it)
+    crossinline action: suspend (T) -> Unit
+) = owner.lifecycleScope.launch(coroutineContext) {
+    owner.lifecycle.repeatOnLifecycle(minActiveState) {
+        collect { action(it) }
     }
 }
 
 /**
- * A convenience wrapper around [addRepeatingJob] that simply calls [collect].
- * Think of it as [kotlinx.coroutines.flow.launchIn], but for collecting.
+ * A convenience wrapper around [androidx.lifecycle.LifecycleCoroutineScope.launch]
+ * that calls [collect], all wrapped in a lifecycle-aware
+ * [repeatOnLifecycle]. Think of it as [kotlinx.coroutines.flow.launchIn], but for
+ * collecting.
  *
  * ```
  * uiStateFlow.collectIn(owner)
@@ -42,6 +47,8 @@ fun <T> Flow<T>.collectIn(
     owner: LifecycleOwner,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
     coroutineContext: CoroutineContext = EmptyCoroutineContext
-) = owner.addRepeatingJob(minActiveState, coroutineContext) {
-    collect()
+) = owner.lifecycleScope.launch(coroutineContext) {
+    owner.lifecycle.repeatOnLifecycle(minActiveState) {
+        collect()
+    }
 }
