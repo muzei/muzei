3deec9533d5829bbe832e9e4cedb5c0f7dd97372
Ian Lake <ian.hannibal.lake@gmail.com>
2025-08-09 21:27:52 -0700

    Start migration to Compose
    Using Material Theme Builder with:
    
    - Primary #3C6CAE
    - Secondary #5999C6
    - Tertiary #F0BD1A
    - Error #FF5449
    - Neutral #1F292A
    - Neutral Variant #626D6E

---
+++ main/src/main/java/com/google/android/apps/muzei/theme/Color.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primaryLight = Color(0xFF1E5394)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFF3C6CAE)
+val onPrimaryContainerLight = Color(0xFFE6EDFF)
+val secondaryLight = Color(0xFF18648E)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFF5999C6)
+val onSecondaryContainerLight = Color(0xFF002F47)
+val tertiaryLight = Color(0xFF765B00)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFF0BD1A)
+val onTertiaryContainerLight = Color(0xFF654D00)
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFFFDAD6)
+val onErrorContainerLight = Color(0xFF93000A)
+val backgroundLight = Color(0xFFF9F9FF)
+val onBackgroundLight = Color(0xFF191C20)
+val surfaceLight = Color(0xFFFBF9F8)
+val onSurfaceLight = Color(0xFF1B1C1C)
+val surfaceVariantLight = Color(0xFFDFE3E3)
+val onSurfaceVariantLight = Color(0xFF434848)
+val outlineLight = Color(0xFF737878)
+val outlineVariantLight = Color(0xFFC3C7C7)
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF303030)
+val inverseOnSurfaceLight = Color(0xFFF3F0EF)
+val inversePrimaryLight = Color(0xFFA7C8FF)
+val surfaceDimLight = Color(0xFFDCD9D9)
+val surfaceBrightLight = Color(0xFFFBF9F8)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFF6F3F2)
+val surfaceContainerLight = Color(0xFFF0EDED)
+val surfaceContainerHighLight = Color(0xFFEAE8E7)
+val surfaceContainerHighestLight = Color(0xFFE4E2E1)
+
+val primaryDark = Color(0xFFA7C8FF)
+val onPrimaryDark = Color(0xFF003061)
+val primaryContainerDark = Color(0xFF3C6CAE)
+val onPrimaryContainerDark = Color(0xFFE6EDFF)
+val secondaryDark = Color(0xFF8ECDFD)
+val onSecondaryDark = Color(0xFF00344E)
+val secondaryContainerDark = Color(0xFF5999C6)
+val onSecondaryContainerDark = Color(0xFF002F47)
+val tertiaryDark = Color(0xFFFFDC86)
+val onTertiaryDark = Color(0xFF3E2E00)
+val tertiaryContainerDark = Color(0xFFF0BD1A)
+val onTertiaryContainerDark = Color(0xFF654D00)
+val errorDark = Color(0xFFFFB4AB)
+val onErrorDark = Color(0xFF690005)
+val errorContainerDark = Color(0xFF93000A)
+val onErrorContainerDark = Color(0xFFFFDAD6)
+val backgroundDark = Color(0xFF111318)
+val onBackgroundDark = Color(0xFFE2E2E8)
+val surfaceDark = Color(0xFF131313)
+val onSurfaceDark = Color(0xFFE4E2E1)
+val surfaceVariantDark = Color(0xFF434848)
+val onSurfaceVariantDark = Color(0xFFC3C7C7)
+val outlineDark = Color(0xFF8D9292)
+val outlineVariantDark = Color(0xFF434848)
+val scrimDark = Color(0xFF000000)
+val inverseSurfaceDark = Color(0xFFE4E2E1)
+val inverseOnSurfaceDark = Color(0xFF303030)
+val inversePrimaryDark = Color(0xFF2D5FA0)
+val surfaceDimDark = Color(0xFF131313)
+val surfaceBrightDark = Color(0xFF393939)
+val surfaceContainerLowestDark = Color(0xFF0E0E0E)
+val surfaceContainerLowDark = Color(0xFF1B1C1C)
+val surfaceContainerDark = Color(0xFF1F2020)
+val surfaceContainerHighDark = Color(0xFF2A2A2A)
+val surfaceContainerHighestDark = Color(0xFF353535)
---
+++ main/src/main/java/com/google/android/apps/muzei/theme/Theme.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2025 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.apps.muzei.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    onPrimary = onPrimaryLight,
+    primaryContainer = primaryContainerLight,
+    onPrimaryContainer = onPrimaryContainerLight,
+    secondary = secondaryLight,
+    onSecondary = onSecondaryLight,
+    secondaryContainer = secondaryContainerLight,
+    onSecondaryContainer = onSecondaryContainerLight,
+    tertiary = tertiaryLight,
+    onTertiary = onTertiaryLight,
+    tertiaryContainer = tertiaryContainerLight,
+    onTertiaryContainer = onTertiaryContainerLight,
+    error = errorLight,
+    onError = onErrorLight,
+    errorContainer = errorContainerLight,
+    onErrorContainer = onErrorContainerLight,
+    background = backgroundLight,
+    onBackground = onBackgroundLight,
+    surface = surfaceLight,
+    onSurface = onSurfaceLight,
+    surfaceVariant = surfaceVariantLight,
+    onSurfaceVariant = onSurfaceVariantLight,
+    outline = outlineLight,
+    outlineVariant = outlineVariantLight,
+    scrim = scrimLight,
+    inverseSurface = inverseSurfaceLight,
+    inverseOnSurface = inverseOnSurfaceLight,
+    inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
+)
+
+private val darkScheme = darkColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surface = surfaceDark,
+    onSurface = onSurfaceDark,
+    surfaceVariant = surfaceVariantDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark,
+    scrim = scrimDark,
+    inverseSurface = inverseSurfaceDark,
+    inverseOnSurface = inverseOnSurfaceDark,
+    inversePrimary = inversePrimaryDark,
+    surfaceDim = surfaceDimDark,
+    surfaceBright = surfaceBrightDark,
+    surfaceContainerLowest = surfaceContainerLowestDark,
+    surfaceContainerLow = surfaceContainerLowDark,
+    surfaceContainer = surfaceContainerDark,
+    surfaceContainerHigh = surfaceContainerHighDark,
+    surfaceContainerHighest = surfaceContainerHighestDark,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable() () -> Unit
+) {
+  val colorScheme = when {
+      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+          val context = LocalContext.current
+          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+      }
+      
+      darkTheme -> darkScheme
+      else -> lightScheme
+  }
+
+  MaterialTheme(
+    colorScheme = colorScheme,
+    content = content
+  )
+}
